80d33d263fcc7d34639bc47caf2e3bf489d765b8
Etim Effanga <66226811+pablo364@users.noreply.github.com>
2021-09-01 08:22:23 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-"use strict";
+import NotificationBadge from './components/NotificationBadge';
+import Effect from './components/Effect';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "Effect", {
-  enumerable: true,
-  get: function get() {
-    return _Effect["default"];
-  }
-});
-exports["default"] = void 0;
-
-var _NotificationBadge = _interopRequireDefault(require("./components/NotificationBadge"));
-
-var _Effect = _interopRequireDefault(require("./components/Effect"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var _default = _NotificationBadge["default"];
-exports["default"] = _default;
+export default NotificationBadge;
+export { Effect };
